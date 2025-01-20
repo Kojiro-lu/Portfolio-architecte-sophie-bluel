d691e028ba6940b4c2c1d35f00ad9243a912feec
anthony <kojiro5656i@gmail.com>
2025-01-20 10:39:49 +0100

    Mise en forme css des bouttons du filtres

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -2,14 +2,14 @@
 const urlApiProjets = "http://localhost:5678/api/works";
 
 //Fonction pour récupèrer les Projets
-async function recuperationProjet() {
+async function recuperationProjets() {
   const reponseProjets = await fetch(urlApiProjets); //Appel des projets avec la variable précèdement créer
   console.log("appel url :", urlApiProjets);
   const projets = await reponseProjets.json(); // Transformation de la réponse en JSON
   console.log("Projets récupérés :", projets); // Affiche les projets pour voir si ok
   return projets; // Renvoie de la variable pour une nouvelle utilisation
 }
-recuperationProjet(); //appel à exécuter la fonction
+recuperationProjets(); //appel à exécuter la fonction
 
 // Affichage des projets dans notre galerie
 function affichageProjets(projets) {
@@ -38,9 +38,66 @@ function affichageProjets(projets) {
     baliseFigcaption.textContent = projets[i].title;
   }
 }
-// Appel de la fonction seulement après récupération des projets avec async pour être sur que le projet soit chargé
+// Appel de la fonction seulement après récupération des projets avec async pour être sur que le projet soit chargé avant secondes étapes
 async function demarrageAffichageProjets() {
-  const projets = await recuperationProjet(); // récup des projets
+  const projets = await recuperationProjets(); // récup des projets
   affichageProjets(projets); // Passer les projets récup à la fonction d'affichage
 }
 demarrageAffichageProjets();
+
+// Fonction génèration des filtres du menu catégorie
+//Adresse de l'API pour les projets
+const urlApiCategories = "http://localhost:5678/api/categories";
+
+//Fonction pour récupérer les catégories
+async function recuperationCategories() {
+  const reponseCategories = await fetch(urlApiCategories); //Appel des categories avec la variable précèdement créer
+  console.log("appel categories :", urlApiCategories);
+  const categories = await reponseCategories.json(); // Transformation de la réponse en JSON
+  console.log("Categories récupérés :", categories); // Affiche les categories pour voir si ok
+  return categories; // Renvoie de la variable pour une nouvelle utilisation
+}
+recuperationCategories(); //appel à exécuter la fonction
+
+//Fonction pour création du menu des filtres catégories
+function menuFiltreCategories(categories, projets) {
+  const menu = document.querySelector(".menu-filtre-categories"); // recuperation du menu dans le DOM
+  menu.innerHTML = ""; // on remet à 0 le menu au cas ou
+
+  //Création du bouton pour tous les projets
+  const boutonTousProjets = document.createElement("button"); //Création du bouton
+  boutonTousProjets.textContent = "Tous"; //texte du bouton
+
+  menu.appendChild(boutonTousProjets); //on ajoute le bouton au menu
+
+  //activation au clique du filtre, pour afficher tous les projets
+  boutonTousProjets.addEventListener("click", () => {
+    affichageProjets(projets);
+  });
+
+  //Création des autres boutons
+  for (let i = 0; i < categories.length; i++) {
+    console.log("categories ok :", categories[i]);
+
+    const boutonCategories = document.createElement("button");
+    boutonCategories.textContent = categories[i].name; // pour le nom de chàque catégories
+
+    menu.appendChild(boutonCategories); // Ajouter les boutons au menu
+
+    //activation au clique du filtre, pour afficher chàque menu suivent sur lequel on clique gràce au id des boutons et des catégories
+    boutonCategories.addEventListener("click", () => {
+      const projetsFiltres = projets.filter(
+        (projet) => projet.categoryId === categories[i].id
+      );
+      affichageProjets(projetsFiltres);
+    });
+  }
+}
+
+//Appel à la fonction quand tout est chargé correctement avec async
+async function demarrageAffichageMenuFiltre() {
+  const categories = await recuperationCategories(); // récup des catégories
+  const projets = await recuperationProjets(); // récupèration également des projets
+  menuFiltreCategories(categories, projets); // Passer les categories et les projets récup à la fonction d'affichage
+}
+demarrageAffichageMenuFiltre();
