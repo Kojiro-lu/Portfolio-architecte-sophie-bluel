42fc3a1fb49484e263bc04315bf5cea197c4db90
anthony <kojiro5656i@gmail.com>
2025-01-30 20:23:46 +0100

    Création dans le fichier modale.js de l'eveneemnt au clique pour la fermeture de la modale

--- FrontEnd/modale/modale.js
+++ FrontEnd/modale/modale.js
@@ -1,11 +1,33 @@
-const openModal = function (e) {
+let modal = null;
+
+const openModal = async function (e) {
   e.preventDefault();
-  const target = document.querySelector(e.target.getAttribut("href"));
-  target.style.display = null;
-  target.removeAttribute("aria-hidden");
-  target.setAttribute("aria-modal, true");
+
+  modal = document.querySelector(e.target.getAttribute("href"));
+  modal.style.display = "flex"; // Ou 'block' si tu préfères
+  modal.removeAttribute("aria-hidden");
+  modal.setAttribute("aria-modal", "true");
+
+  // Empêcher la fermeture si on clique sur le contenu de la modal
+  modal
+    .querySelector(".block-modal")
+    .addEventListener("click", (e) => e.stopPropagation());
+
+  modal.addEventListener("click", closeModal);
+  modal.querySelector(".modal-close").addEventListener("click", closeModal);
 };
 
-document.querySelector("btn-modifier").forEach((a) => {
-  a.addEventListener("click", openModal);
+const closeModal = function (e) {
+  if (modal === null) return;
+  e.preventDefault();
+  modal.style.display = "none";
+  modal.setAttribute("aria-hidden", "true");
+  modal.removeAttribute("aria-modal");
+  modal.removeEventListener("click", closeModal);
+  modal.querySelector(".modal-close").removeEventListener("click", closeModal);
+  modal = null;
+};
+
+document.querySelectorAll(".btn-modifier").forEach((btn) => {
+  btn.addEventListener("click", openModal);
 });
