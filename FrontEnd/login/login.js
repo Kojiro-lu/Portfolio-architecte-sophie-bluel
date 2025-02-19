d4da49dedd788c7bc399ac3581896c8488cbd357
Anthony <kojiro5656i@gmail.com>
2025-02-19 13:28:30 +0100

    clean de certains commentaires sans intêret dans login.js et script.js

--- FrontEnd/login/login.js
+++ FrontEnd/login/login.js
@@ -7,18 +7,17 @@ async function apiLoginRequest(email, password) {
 
   try {
     const response = await fetch(urlApiLogin, {
-      method: "POST", //type de requête post pour l'envoie des données à l'api
-      headers: { "Content-Type": "application/json" }, //requête en json
-      body: JSON.stringify(loginData), //convertir loginData en json pour l'envoie de la requête
+      method: "POST", 
+      headers: { "Content-Type": "application/json" }, 
+      body: JSON.stringify(loginData), 
     });
 
-    const responseBody = await response.json(); //stocké la réponse dans la constante
+    const responseBody = await response.json(); 
 
     if (!response.ok) {
-      //vérification de la réponse et si erreur alors
-      throw new Error( //gràce à throw on créer l'erreur dans réponse status
+      throw new Error( 
         `HTTP Error: ${response.status} - ${
-          responseBody.message || "Erreur inconnue" //message d'erreur ou erreur iconnue
+          responseBody.message || "Erreur inconnue" 
         }`
       );
     }
@@ -32,35 +31,34 @@ async function apiLoginRequest(email, password) {
 
 // Fonction pour gérer la réponse ok aprés tentative de connexion
 function handleLoginResponse(data) {
-  const token = data.token; // on récupère le token transmis
-  localStorage.setItem("authToken", token); //on stock le tocken dans le localStorage de l'ordinateur
-  window.location.href = "../index.html"; // Redirection vers la page d'accueil
+  const token = data.token; 
+  localStorage.setItem("authToken", token); 
+  window.location.href = "../index.html"; 
 }
 
 // Fonction pour afficher un message d'erreur si tentative de connexion ko
 function displayErrorMessage(message) {
   const errorMessage = document.querySelector("#message-erreur-connexion");
-  errorMessage.textContent = message; //on affiche notre message d'erreur récupérer
+  errorMessage.textContent = message; 
 }
 
 // Fonction pour gérer la soumission du formulaire de connexion
 async function handleLogin(event) {
-  event.preventDefault(); //on enpêche le rechargement par dêfaut de la page
+  event.preventDefault(); 
 
-  const email = document.querySelector("#mail").value.trim(); //on récupère ce qu'on a écris dans la case email
-  const password = document.querySelector("#password").value.trim(); //on récupère ce qu'on a écris dans la case password
+  const email = document.querySelector("#mail").value.trim(); 
+  const password = document.querySelector("#password").value.trim(); 
 
   if (!email || !password) {
-    displayErrorMessage("Merci de remplir les 2 champs"); //message d'erreur si un des 2 champs n'est pas remplie
+    displayErrorMessage("Merci de remplir les 2 champs");
     return;
   }
 
   //utilisation de try/catch pour l'affichage d'une erreur si mots de passe ou mail invalide
   try {
     const data = await apiLoginRequest(email, password);
-    handleLoginResponse(data); //si ok pas de probléme
+    handleLoginResponse(data); 
   } catch (error) {
-    //si ko on affiche le message d'erreur
     displayErrorMessage(
       "Email ou mot de passe invalide, merci de saisir de nouveau vos informations de connexion."
     );
@@ -70,11 +68,10 @@ async function handleLogin(event) {
 // Ajouter l'événement au formulaire
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector(".formulaire-login");
-  //on vérifie d'abord que le formulaire existe bien
   if (!form) {
     console.error("Le formulaire n'a pas été trouvé dans le DOM.");
     return;
   }
 
-  form.addEventListener("submit", handleLogin); //écouteur pour le submint du formulaire qui appel la fonction handlelogin
+  form.addEventListener("submit", handleLogin);
 });
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -10,13 +10,13 @@ export const urlApiProjects = "http://localhost:5678/api/works";
 // Fonction pour récupérer les projets
 export async function getProjects() {
   try {
-    const answerProjects = await fetch(urlApiProjects); // Appel des projets avec la variable précédemment créée
+    const answerProjects = await fetch(urlApiProjects); 
 
     if (!answerProjects.ok) {
-      throw new Error(`Erreur HTTP : ${answerProjects.status}`); // Vérification si la réponse est ok ou non
+      throw new Error(`Erreur HTTP : ${answerProjects.status}`); 
     }
 
-    return await answerProjects.json(); // Transformation de la réponse en JSON et retour des projets
+    return await answerProjects.json(); 
   } catch (error) {
     console.error(
       "Erreur lors de la récupération des projets :",
@@ -25,7 +25,7 @@ export async function getProjects() {
   }
 }
 
-getProjects(); // Appel à exécuter la fonction
+getProjects(); 
 
 // Création des fonctions pour les cards, images, titles.
 function createProjectCard() {
@@ -34,37 +34,35 @@ function createProjectCard() {
 
 function createProjectImage(imageUrl, title, card) {
   const img = document.createElement("img");
-  img.src = imageUrl; // URL de l'image
-  img.alt = title; // Titre du projet qui sert au titre de l'image
+  img.src = imageUrl; 
+  img.alt = title; 
   card.appendChild(img);
 }
 
 function createProjectTitle(title, card) {
   const figcaption = document.createElement("figcaption");
-  figcaption.textContent = title; // Titre du projet
+  figcaption.textContent = title; 
   card.appendChild(figcaption);
 }
 
 // Affichage des projets dans notre galerie
 function displayProjects(projects) {
-  const gallery = document.querySelector(".gallery"); // On récupère la galerie dans le DOM
-  gallery.innerHTML = ""; // Remettre à 0 les éléments présents au cas où
+  const gallery = document.querySelector(".gallery"); 
+  gallery.innerHTML = ""; 
 
   // On lance une boucle forEach pour parcourir le tableau projects
   projects.forEach((project) => {
     const card = createProjectCard();
     createProjectImage(project.imageUrl, project.title, card);
     createProjectTitle(project.title, card);
-
-    // On raccroche chaque enfant à son parent pour l'affichage au bon endroit
     gallery.appendChild(card);
   });
 }
 
 // Appel de la fonction seulement après récupération des projets avec async
 async function startDisplayProjects() {
-  const projects = await getProjects(); // Récupération des projets
-  if (projects) displayProjects(projects); // Vérification et affichage des projets
+  const projects = await getProjects(); 
+  if (projects) displayProjects(projects); 
 }
 
 startDisplayProjects();
@@ -77,9 +75,9 @@ export async function refreshGallery() {
 
 //////////////////////////////////////////////////////////
 //                                                     //
-// Fonction génération des filtres du menu catégorie  //
+// Fonction génération des filtres du menu catégorie   //
 //                                                     //
-//////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
 
 // Adresse de l'API pour les catégories
 const urlApiCategories = "http://localhost:5678/api/categories";
@@ -93,7 +91,7 @@ export async function getCategories() {
       throw new Error(`Erreur HTTP : ${answerCategories.status}`);
     }
 
-    return await answerCategories.json(); // Retourne les catégories
+    return await answerCategories.json(); 
   } catch (error) {
     console.error(
       "Erreur lors de la récupération des catégories :",
@@ -132,7 +130,7 @@ function addFilterEvent(buttonCategories, categoryId, projects) {
 // Fonction pour la création du menu des filtres catégories
 function menuFilterCategories(categories, projects) {
   const menu = document.querySelector(".menu-filter-categories");
-  menu.innerHTML = ""; // Réinitialisation du menu
+  menu.innerHTML = ""; 
 
   // Création du bouton pour afficher tous les projets
   const buttonAllProjects = document.createElement("button");
@@ -179,23 +177,23 @@ startDisplayMenuProjects();
 // Mise à jour du bouton login/logout
 function checkLoginStatus() {
   const token = localStorage.getItem("authToken");
-  const loginLink = document.querySelector("nav ul li:nth-child(3) a"); // Cibler le lien "login"
+  const loginLink = document.querySelector("nav ul li:nth-child(3) a"); 
 
   if (token) {
-    loginLink.textContent = "Logout"; // Changer le texte en "Logout"
-    loginLink.href = "#"; // Désactiver le lien de redirection
-    loginLink.addEventListener("click", handleLogout); // Ajouter l'événement de déconnexion
+    loginLink.textContent = "Logout"; 
+    loginLink.href = "#"; 
+    loginLink.addEventListener("click", handleLogout); 
   } else {
-    loginLink.textContent = "Login"; // Laisser le login pour se connecter
-    loginLink.href = "./login/login.html"; // Rediriger vers la page de connexion
+    loginLink.textContent = "Login"; 
+    loginLink.href = "./login/login.html"; 
   }
 }
 
 // Fonction pour la déconnexion
 function handleLogout(event) {
-  event.preventDefault(); // Empêcher le comportement par défaut du lien
-  localStorage.removeItem("authToken"); // Supprimer le token
-  window.location.reload(); // Recharger la page
+  event.preventDefault(); 
+  localStorage.removeItem("authToken"); 
+  window.location.reload(); 
 }
 
 // Exécuter au chargement de la page pour vérifier la connexion
@@ -211,9 +209,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Vérification de la connexion pour afficher le bandeau
 function checkLoginStatusForDisplayBanner() {
-  const token = localStorage.getItem("authToken"); // Vérifier le token dans localStorage
-  const bandeau = document.querySelector(".bandeau-noir"); // Cibler le bandeau noir dans le DOM
-  const bodyConnected = document.body; // Sélection du body pour l'ajout d'un margin-top dynamique
+  const token = localStorage.getItem("authToken"); 
+  const bandeau = document.querySelector(".bandeau-noir"); 
+  const bodyConnected = document.body; 
 
   if (token && bandeau) {
     bandeau.style.display = "flex";
@@ -234,18 +232,18 @@ document.addEventListener("DOMContentLoaded", () => {
 ///////////////////////////////////////////////////////////////////////////////////////
 
 function checkLoginStatusForDeletedFilter() {
-  const token = localStorage.getItem("authToken"); // Vérifier que le token est bien présent dans localStorage
-  const menuFilter = document.querySelector(".menu-filter-categories"); // Sélectionner la classe des filtres
-  const buttonModifyProjets = document.querySelector(".modifier-projet"); // Sélectionner la classe du bouton modifier
-  const myProjects = document.querySelector(".mes-projets"); // Sélectionner la section des projets
+  const token = localStorage.getItem("authToken"); 
+  const menuFilter = document.querySelector(".menu-filter-categories"); 
+  const buttonModifyProjets = document.querySelector(".modifier-projet"); 
+  const myProjects = document.querySelector(".mes-projets"); 
 
   if (token) {
-    menuFilter.style.display = "none"; // Cacher les filtres
-    buttonModifyProjets.style.display = "flex"; // Afficher le bouton modifier
+    menuFilter.style.display = "none"; 
+    buttonModifyProjets.style.display = "flex"; 
     myProjects.style.flexDirection = "row";
   } else {
-    menuFilter.style.display = "flex"; // Afficher les filtres
-    buttonModifyProjets.style.display = "none"; // Cacher le bouton modifier
+    menuFilter.style.display = "flex"; 
+    buttonModifyProjets.style.display = "none"; 
     myProjects.style.flexDirection = "column";
   }
 }
